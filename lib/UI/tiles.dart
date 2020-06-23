b9ea1c2b461bc2c9f3a1f37856335d0fae7a5999
Akash Kashyap <kashyapakash167@gmail.com>
2020-06-24 02:16:25 +0530

    updated the bars.

--- lib/UI/tiles.dart
+++ lib/UI/tiles.dart
@@ -2,92 +2,94 @@ import 'package:flutter/material.dart';
 import 'package:imaginesorting/UI/widgets/custom_tile.dart';
 
 class Tiles extends StatelessWidget {
-  final List<int> list = [15, 35, 45, 21, 23, 42, 62, 46, 82, 75, 67, 48, 59, 35, 41, 27, 38, 19, 84, 65];
+  final List<int> list = [150, 350, 450, 210, 230, 420, 620, 460, 820, 750, 670, 480, 590, 350, 410, 270, 380, 190, 840, 650];
 
   @override
   Widget build(BuildContext context) {
+
+    final _height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
           children: [
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[0],),
+            CustomTile(height:list[0],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[1],),
+            CustomTile(height: list[1],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[2],),
+            CustomTile(height: list[2],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[3],),
+            CustomTile(height: list[3],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[4],),
+            CustomTile(height: list[4],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[5],),
+            CustomTile(height: list[5],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[6],),
+            CustomTile(height: list[6],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[7],),
+            CustomTile(height: list[7],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[8],),
+            CustomTile(height: list[8],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[9],),
+            CustomTile(height: list[9],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[10],),
+            CustomTile(height: list[10],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[11],),
+            CustomTile(height: list[11],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[12],),
+            CustomTile(height: list[12],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[13],),
+            CustomTile(height: list[13],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[14],),
+            CustomTile(height: list[14],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[15],),
+            CustomTile(height: list[15],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[16],),
+            CustomTile(height: list[16],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[17],),
+            CustomTile(height: list[17],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[18],),
+            CustomTile(height: list[18],),
             SizedBox(
               width: 5.0,
             ),
-            CustomTile(height: MediaQuery.of(context).size.height /2 - list[19],),
+            CustomTile(height: list[19],),
             SizedBox(
               width: 5.0,
             ),
--- lib/UI/widgets/custom_tile.dart
+++ lib/UI/widgets/custom_tile.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 
 class CustomTile extends StatelessWidget {
 
-  final double height;
+  final int height;
 
   CustomTile({this.height});
 
   @override
   Widget build(BuildContext context) {
+
+    final _height = MediaQuery.of(context).size.height;
+
     return Align(
       alignment: Alignment.topCenter,
           child: Container(
         width: 15.0,
-        height: height,
+        height: _height - height,
         color: Colors.red,
       ),
     );
